3847d03bc8dae96e0cfeb785f9fff057f899ea90
nani <adnaan.velji@gmail.com>
2024-04-02 09:09:52 +0100

    init

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"text/template"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	e := echo.New()
+
+	t := &Template{
+		templates: template.Must(template.ParseGlob("templates/*.html")),
+	}
+	e.Renderer = t
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", nil)
+		// return c.String(http.StatusOK, "hello world")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
+}
