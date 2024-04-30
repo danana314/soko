d3d91dcbee69c30cd218b8607de5563911cf4bbd
nani <adnaan.velji@gmail.com>
2024-04-30 23:46:24 +0100

    working example from primeagen tutorial - building contacts and address book

--- main.go
+++ main.go
@@ -2,21 +2,108 @@ package main
 
 import (
 	"1008001/splitwiser/middleware"
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
-func handleIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
+var templates = template.Must(template.ParseGlob("templates/*.tmpl"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data any) {
+	err := templates.ExecuteTemplate(w, tmpl, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+type Contact struct {
+	Name  string
+	Email string
+}
+
+func newContact(name, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func (d *Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("John", "jd@gmail.com"),
+			newContact("Clara", "cd@gmail.com"),
+		},
+	}
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
 }
 
 func main() {
 	router := http.NewServeMux()
-	router.HandleFunc("GET /", handleIndex)
+
+	page := newPage()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "index", page)
+	})
+
+	router.HandleFunc("POST /contacts", func(w http.ResponseWriter, r *http.Request) {
+		name := r.FormValue("name")
+		email := r.FormValue("email")
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+
+			// renderTemplate(w, http.StatusUnprocessableEntity, "form", formData)
+			http.Error(w, "email already exists", http.StatusUnprocessableEntity)
+			return
+		}
+		page.Data.Contacts = append(page.Data.Contacts, newContact(name, email))
+		renderTemplate(w, "display", page.Data)
+	})
 
 	server := http.Server{
-		Addr:    "8080",
+		Addr:    "localhost:8080",
 		Handler: middleware.Logging(router),
 	}
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 }
--- middleware/logging.go
+++ middleware/logging.go
@@ -6,27 +6,12 @@ import (
 	"time"
 )
 
-type wrappedWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (w *wrappedWriter) WriteHeader(statusCode int) {
-	w.ResponseWriter.WriteHeader(statusCode)
-	w.statusCode = statusCode
-}
-
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
-		wrapped := &wrappedWriter{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
+		next.ServeHTTP(w, r)
 
-		next.ServeHTTP(wrapped, r)
-
-		log.Println(wrapped.statusCode, r.Method, r.URL.Path, time.Since(start))
+		log.Println(r.Method, r.URL.Path, time.Since(start))
 	})
 }
