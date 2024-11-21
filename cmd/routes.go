b45ed5e2a03b8bc58a366431f46eb21b700aaca6
nani <adnaan.velji@gmail.com>
2024-11-21 21:51:11 +0000

    split out route handlers into named functions

--- cmd/routes.go
+++ cmd/routes.go
@@ -16,6 +16,8 @@ import (
 	"github.com/urfave/negroni"
 )
 
+var router *http.ServeMux
+var templates *template.Template
 var decoder = schema.NewDecoder()
 
 func renderTemplate(t *template.Template, w http.ResponseWriter, tmpl string, data any) {
@@ -32,104 +34,115 @@ var timeConverter = func(value string) reflect.Value {
 	return reflect.Value{} // this is the same as the private const invalidType
 }
 
-func routes() http.Handler {
-	router := http.NewServeMux()
+func init() {
+	router = http.NewServeMux()
 	decoder.RegisterConverter(time.Time{}, timeConverter)
-
-	fileServer := http.FileServer(http.FS(web.EmbeddedFiles))
-	router.Handle("GET /static/", fileServer)
-
 	// var templates = template.Must(template.ParseGlob("web/templates/*.tmpl")).Funcs(funcs.TemplateFuncs)
-	templates, err := template.New("").Funcs(funcs.TemplateFuncs).ParseFS(web.EmbeddedFiles, "templates/*.tmpl")
+	var err error
+	templates, err = template.New("").Funcs(funcs.TemplateFuncs).ParseFS(web.EmbeddedFiles, "templates/*.tmpl")
 	if err != nil {
 		slog.Error(err.Error())
 	}
-	router.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
-		renderTemplate(templates, w, "index", nil)
-	})
+}
 
-	router.HandleFunc("POST /t/new", func(w http.ResponseWriter, r *http.Request) {
-		trip := models.NewTrip()
-		store.SaveTripDetails(trip)
-		http.Redirect(w, r, fmt.Sprintf("/t/%s", trip.Id), http.StatusSeeOther)
-	})
+func routes() http.Handler {
+	fileServer := http.FileServer(http.FS(web.EmbeddedFiles))
+	router.Handle("GET /static/", fileServer)
 
-	router.HandleFunc("GET /t/{tripId}", func(w http.ResponseWriter, r *http.Request) {
-		tripId := r.PathValue("tripId")
-		trip := store.GetTrip(tripId)
-		if trip == nil {
-			http.Error(w, "trip not found", http.StatusNotFound)
-		} else {
-			renderTemplate(templates, w, "trip", trip)
-		}
-	})
-
-	router.HandleFunc("POST /t/{tripId}", func(w http.ResponseWriter, r *http.Request) {
-		tripId := r.PathValue("tripId")
-		err := r.ParseForm()
-		if err != nil {
-			slog.Error(err.Error())
-		}
-
-		trip := new(models.Trip)
-		trip.Id = tripId
-		err = decoder.Decode(trip, r.PostForm)
-		if err != nil {
-			slog.Error(err.Error(), "postform", r.PostForm)
-		}
-		store.SaveTripDetails(trip)
-		trip = store.GetTrip(trip.Id)
-		renderTemplate(templates, w, "trip_detail", trip)
-	})
-
-	router.HandleFunc("POST /t/{tripId}/u", func(w http.ResponseWriter, r *http.Request) {
-		tripId := r.PathValue("tripId")
-		trip := store.GetTrip(tripId)
-
-		name := r.PostFormValue("name")
-		user := models.NewUser(name)
-		store.AddUser(tripId, user)
-		trip.Users = append(trip.Users, *user)
-		renderTemplate(templates, w, "trip_detail", trip)
-	})
-
-	router.HandleFunc("POST /t/{tripId}/s", func(w http.ResponseWriter, r *http.Request) {
-		tripId := r.PathValue("tripId")
-		err := r.ParseForm()
-		if err != nil {
-			slog.Error(err.Error())
-		}
-		trip := store.GetTrip(tripId)
-		for k := range r.PostForm {
-			se, err := trip.NewScheduleEntry(k)
-			if err != nil {
-				slog.Error(err.Error())
-			}
-			trip.Schedule = append(trip.Schedule, *se)
-		}
-		store.SaveSchedule(trip)
-		renderTemplate(templates, w, "trip_detail", trip)
-	})
-
-	router.HandleFunc("POST /t/{tripId}/e", func(w http.ResponseWriter, r *http.Request) {
-		tripId := r.PathValue("tripId")
-		err := r.ParseForm()
-		if err != nil {
-			slog.Error(err.Error())
-		}
-		expense := new(models.Expense)
-		err = decoder.Decode(expense, r.PostForm)
-		if err != nil {
-			slog.Error(err.Error(), "postform", r.PostForm)
-		}
-		trip := store.GetTrip(tripId)
-		trip.Expenses = append(trip.Expenses, *expense)
-		slog.Info(fmt.Sprintf("%#v", trip))
-		renderTemplate(templates, w, "trip_detail", trip)
-	})
+	router.HandleFunc("GET /{$}", Index)
+	router.HandleFunc("POST /t/new", NewTrip)
+	router.HandleFunc("GET /t/{tripId}", GetTrip)
+	router.HandleFunc("POST /t/{tripId}", UpdateTrip)
+	router.HandleFunc("POST /t/{tripId}/u", UpdateUser)
+	router.HandleFunc("POST /t/{tripId}/s", UpdateSchedule)
+	router.HandleFunc("POST /t/{tripId}/e", NewExpense)
 
 	n := negroni.Classic() // default middleware: panic recovery, logger, static serving
 	n.UseHandler(router)
 
 	return n
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(templates, w, "index", nil)
+}
+
+func NewTrip(w http.ResponseWriter, r *http.Request) {
+	trip := models.NewTrip()
+	store.SaveTripDetails(trip)
+	http.Redirect(w, r, fmt.Sprintf("/t/%s", trip.Id), http.StatusSeeOther)
+}
+
+func GetTrip(w http.ResponseWriter, r *http.Request) {
+	tripId := r.PathValue("tripId")
+	trip := store.GetTrip(tripId)
+	if trip == nil {
+		http.Error(w, "trip not found", http.StatusNotFound)
+	} else {
+		renderTemplate(templates, w, "trip", trip)
+	}
+}
+
+func UpdateTrip(w http.ResponseWriter, r *http.Request) {
+	tripId := r.PathValue("tripId")
+	err := r.ParseForm()
+	if err != nil {
+		slog.Error(err.Error())
+	}
+
+	trip := new(models.Trip)
+	trip.Id = tripId
+	err = decoder.Decode(trip, r.PostForm)
+	if err != nil {
+		slog.Error(err.Error(), "postform", r.PostForm)
+	}
+	store.SaveTripDetails(trip)
+	trip = store.GetTrip(trip.Id)
+	renderTemplate(templates, w, "trip_detail", trip)
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	tripId := r.PathValue("tripId")
+	trip := store.GetTrip(tripId)
+
+	name := r.PostFormValue("name")
+	user := models.NewUser(name)
+	store.AddUser(tripId, user)
+	trip.Users = append(trip.Users, *user)
+	renderTemplate(templates, w, "trip_detail", trip)
+}
+
+func UpdateSchedule(w http.ResponseWriter, r *http.Request) {
+	tripId := r.PathValue("tripId")
+	err := r.ParseForm()
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	trip := store.GetTrip(tripId)
+	for k := range r.PostForm {
+		se, err := trip.NewScheduleEntry(k)
+		if err != nil {
+			slog.Error(err.Error())
+		}
+		trip.Schedule = append(trip.Schedule, *se)
+	}
+	store.SaveSchedule(trip)
+	renderTemplate(templates, w, "trip_detail", trip)
+}
+
+func NewExpense(w http.ResponseWriter, r *http.Request) {
+	tripId := r.PathValue("tripId")
+	err := r.ParseForm()
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	expense := new(models.Expense)
+	err = decoder.Decode(expense, r.PostForm)
+	if err != nil {
+		slog.Error(err.Error(), "postform", r.PostForm)
+	}
+	trip := store.GetTrip(tripId)
+	trip.Expenses = append(trip.Expenses, *expense)
+	slog.Info(fmt.Sprintf("%#v", trip))
+	renderTemplate(templates, w, "trip_detail", trip)
 }
